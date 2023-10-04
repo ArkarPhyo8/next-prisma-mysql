53f81e2cd1d5b9c88e365087ebd686f500fa0e9d
ArkarPhyo8 <arkarphyo200156@gmail.com>
2023-10-04 22:07:01 +0630

    delete components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,3 @@
-import Table from "@/components/Table";
-import WorkListPage from "@/components/WorkListPage";
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <main className="container px-20 mt-8">
-      <WorkListPage />
-      <div className="mt-20">
-        <Table />
-      </div>
-    </main>
-  );
+  return <main>Hello</main>;
 }
